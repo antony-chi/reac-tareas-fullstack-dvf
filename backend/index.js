70df51f6581a266304a5f042793837019dcec4b8
antony-chi <antony.cp9@gmail.com>
2023-06-01 02:18:48 -0500

    conexion a la base mongo atlas

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ import {config} from "dotenv";
 config();
 const port = process.env.PORT|| 3000//metodo de dotenv variable de entonrno
 //---importamos la conexion a la base
-
+import connectDB from "./database.js";
+connectDB();//llamamos a la funcion de la base
 //llamamos la conecion
 app.listen(port);
 console.log("server listen port ", port)
