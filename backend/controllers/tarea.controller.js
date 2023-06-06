38a1780620a99ed68a4185d9839896837074bb4a
antony-chi <antony.cp9@gmail.com>
2023-06-06 15:07:00 -0600

    se modifico controller se aplico CRUD

--- backend/controllers/tarea.controller.js
+++ backend/controllers/tarea.controller.js
@@ -7,30 +7,50 @@ export const getTareas = asyncHandler(async (req, res) => {
 });
 
 export const createTareaNueva = asyncHandler(async (req, res) => {
-  const {title, description} = req.body
-  if(!title || !description){ return res.json({mensage: "missinng info"})}
-  
+  const { title, description } = req.body;
+
+  if (!title || !description) {
+    res.status(400);
+    throw new Error("Please insert title and description");
+  }
 
   const newTarea = {
     title: title,
-    description: description
-  }
-  const tareaSave = Tareas(newTarea)//pasamos al modelo
-  //const tareaDb = await tareaSave.save()
-  console.log(tareaSave)
+    description: description,
+  };
+  const tareaSave = Tareas(newTarea); //pasamos al modelo
+  const tareaSaved = await tareaSave.save(); //guarda en la db
 
-  res.status(201).json(tareaSave)
-  }
-);
+  res.status(201).json(tareaSaved);
+});
 
 export const UpdateTareaEditar = asyncHandler(async (req, res) => {
   const id = req.params.id;
-  console.log(id);
-  res.status(404).json("se actualizo " + id);
+
+  const IdFound = await Tareas.findById(id);
+  if (!IdFound) {
+    res.status(404);
+    throw new Error("Id not Found");
+  }
+  const tareaUpdate = await Tareas.findByIdAndUpdate(id, req.body, {
+    new: true,
+  });
+  console.log(tareaUpdate);
+  res.json(tareaUpdate);
 });
 
 export const deleteTarea = asyncHandler(async (req, res) => {
   const id = req.params.id;
-  console.log(id);
-  res.status(404).json("eliminar tarea " + id);
+
+  
+  const idDelete = await Tareas.findById(id)
+  console.log(idDelete)
+  
+  if (!idDelete) {
+    res.status(404);
+    throw new Error("Id no found, can not deleted");
+  }
+  await Tareas.findByIdAndDelete(id);
+  
+  res.status(200).json(`Id ${id} deleted successfully`);
 });
