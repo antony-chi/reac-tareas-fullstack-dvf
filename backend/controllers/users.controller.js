a90f0f7babb07b1c27403ae905b95d5c8a607f6c
antony-chi <antony.cp9@gmail.com>
2023-06-08 17:43:16 -0600

    crear usuario complet

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -1,11 +1,52 @@
-export const createUser = (req, res) => {
-  res.json("creando usuario");
-};
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+import asynchandler from "express-async-handler";
+import Users from "../models/userModel.js";
 
-export const loginUser = (req, res) => {
+export const createUser = asynchandler(async (req, res) => {
+  const { name, email, password } = req.body;
+    //validamos que existe los datos necesarios
+  if (!name || !email || !password) {
+    res.status(400);
+    throw new Error("Faltan datos");
+  }
+
+  //validamos el usuro existe
+  const userFound = await Users.findOne({ email });
+  //console.log(userFound)
+  if (userFound) {
+    res.status(400);
+    throw new Error("email registered");
+  }
+  //generamos el salt
+  const salt = await bcrypt.genSalt(10);
+  //generamos el hash
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  const userNew = {
+    name: name,
+    email: email,
+    password: hashedPassword,
+  };
+  const userSave = await Users(userNew); //pasamos al modelo
+  const userSaved = await userSave.save(); //giradamos en la DB
+
+  if (userSaved) {
+    res.status(201).json({
+      id: userSaved._id,
+      name: userSaved.name,
+      email: userSaved.email,
+    });
+  } else {
+    res.st(404)
+    throw new Error("Data incorrect, no created the user")
+  }
+});
+
+export const loginUser = asynchandler(async (req, res) => {
   res.json("login");
-};
+});
 
-export const misDatos = (req, res) => {
+export const misDatos = asynchandler(async (req, res) => {
   res.json("mis datos");
-};
+});
