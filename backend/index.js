6e585640a4b7cfdbda692941b55eafc45e5243b2
antony-chi <antony.cp9@gmail.com>
2023-06-14 01:38:09 -0600

    change port 5000

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import {config} from "dotenv";
 config();
 const port = process.env.PORT //metodo de dotenv variable de entonrno
 //---importamos la conexion a la base
-import connectDB from "../backend/config/database.js";
+import connectDB from "./config/database.js";
 connectDB();//llamamos a la funcion de la base
 //llamamos la conecion
 app.listen(port);
